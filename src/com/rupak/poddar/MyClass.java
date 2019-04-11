c86b966b6060390fdea8596febf598dce94b107b
Rupak Poddar <fin.rupak1983@gmail.com>
2019-04-11 13:08:28 +0300

    One line added

--- src/com/rupak/poddar/MyClass.java
+++ src/com/rupak/poddar/MyClass.java
@@ -5,6 +5,8 @@ public class MyClass {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, welcome to GIT learning session");
+		
+		System.out.println("My name is Rupak Poddar");
 
 	}
 
