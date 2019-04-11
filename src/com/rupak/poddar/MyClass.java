45b3dde308048944314a89a90770d07707f9936f
Rupak Poddar <fin.rupak1983@gmail.com>
2019-04-11 13:09:20 +0300

    one blank line deleted

--- src/com/rupak/poddar/MyClass.java
+++ src/com/rupak/poddar/MyClass.java
@@ -5,7 +5,6 @@ public class MyClass {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, welcome to GIT learning session");
-		
 		System.out.println("My name is Rupak Poddar");
 
 	}
